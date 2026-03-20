4690d890a3abb9c58cc3c25025bf7a1aed4c7a9d
MagonxESP <janma.360@gmail.com>
2026-03-20 19:44:44 +0100

    docs: spanish trasnalation

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -32,7 +32,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'es'],
   },
 
   presets: [
@@ -88,6 +88,10 @@ const config: Config = {
         {
           href: 'https://github.com/mikupush',
           label: 'GitHub',
+          position: 'right',
+        },
+        {
+          type: 'localeDropdown',
           position: 'right',
         },
       ],
